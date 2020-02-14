35d8a6d50d11acd1250b438d956ba2922b4c6a5c
RachNovo <r.novoselac@gmail.com>
2020-02-14 09:25:49 -0500

    re-arranging paths for heroku deployment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const db = require('../database/db.js');
+const db = require('./database/db.js');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '/./client/dist'));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
